bfc2cb926d616a0d714ed067f61a2c80a789d538
Owen O'Connor <oconnor.57@gmail.com>
2020-05-21 23:45:54 -0400

    More files

--- installer.sh
+++ installer.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-ANACONDA_LINK='https://repo.anaconda.com/archive/Anaconda3-2020.02-Linux-x86_64.sh'
+ANACONDA_URL='https://repo.anaconda.com/archive/Anaconda3-2020.02-Linux-x86_64.sh'
+ARDUINO_URL='https://downloads.arduino.cc/arduino-1.8.12-linux64.tar.xz'
+
+TTY_GROUP='dialout'
+
+echo "Please enter the username for the student: "
+read username
+
+adduser --shell '/bin/bash' $username
+
+mkdir /tmp/install/
+
+
 
 # Add all of the necessary external repositories
 # adobe flash player
@@ -19,9 +31,24 @@ apt install okular
 
 # Install anaconda
 apt install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6
-wget $ANACONDA_LINK -O /tmp/anacondaInstaller.sh
-chmod 0755 /tmp/anacondaInstaller.sh
+wget $ANACONDA_URL -O /tmp/install/anacondaInstaller.sh
+chmod 0755 /tmp/install/anacondaInstaller.sh
 echo -n 'Enter a username to install anaconda for: '
 read username
 runuser -l $username -c 'sh /tmp/anacondaInstaller.sh'
-rm /tmp/anacondaInstaller
+rm /tmp/install/anacondaInstaller
+
+# Install arduino
+wget $ARDUINO_URL -O /tmp/install/arduino.tar.xz
+tar xf /tmp/install/arduino.tar.xz
+runuser -l $username -c 'sh /tmp/install/arduino*/install.sh'
+usermod -aG $TTY_GROUP $username
+rm -rf /tmp/install/arduino*/*
+rmdir /tmp/install/arduino*
+rm /tmp/install/arduino.tar.xz
+
+# Install chromium
+apt install chromium-browser
+
+
+
